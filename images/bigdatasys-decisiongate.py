f905f5df0a8a8b9213c1fcbb234780a660c40327
majewm <drmacmaj@gmail.com>
2023-03-02 19:06:01 +0100

    update

--- images/bigdatasys-decisiongate.py
+++ images/bigdatasys-decisiongate.py
@@ -1,2 +1,12 @@
 print('bigdatasys-decisiongate')
 print('contents:')
+# Dashboard for IPYNB repositories:
+# Dashboard for statistical analysis and statistical modeling:
+# Dashboard for Industrial Internet of Things (IoT) data analysis:
+# Dashboard for Digital Twins with data models and machine learning models:
+# Dashboard for machine learning models and statistical measures:
+# Dashboard for data assimilation, modeling and visualization:
+# Dashboard for visualization of data sets of process quality:
+# Dashboard for supply chain analysis - production and transportation scheduling using Digital Twins & machine learning:
+# Dashboard for machine learning:
+# Dashboard for machine learning methods:
